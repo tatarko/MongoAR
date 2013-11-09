414e3e32dfed0869784f0b7b09b831296ea9194e
Tomáš Tatarko <tomas@tatarko.sk>
2013-11-09 10:32:40 +0100

    phpdoc filled for Exception class

--- source/Exception.php
+++ source/Exception.php
@@ -5,9 +5,17 @@ namespace MongoAR;
 use Exception as BasicException;
 
 /**
- * Description of Expcetion
- *
- * @author tomastatarko
+ * Custom Expcetion
+ * 
+ * Whole MongoDB Active Record throws
+ * this exception in case of some error.
+ * So you can easily track errors coming
+ * from ActiveRecord itself.
+ * 
+ * @since 0.1
+ * @author Tomas Tatarko <tomas@tatarko.sk>
+ * @license http://choosealicense.com/licenses/mit/ The MIT License
+ * @link https://github.com/tatarko/MongoAR Official github repo
  */
 class Exception extends BasicException
 {
